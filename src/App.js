5cfff877b684473e84d472c31d790bf09ba5f183
Otto <otto.laitinen@edu.turkuamk.fi>
2022-02-02 09:57:06 +0200

    install dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+
+    return (
+        <div>
+            <hi>Testin testin</hi>
+        </div>
+    );
+}
+
+// export default is used to export a single class, function or primitive from a script file
+export default App;
