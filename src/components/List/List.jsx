49ae076bad54c396383d6fe7d2d09a51ea30a34a
Otto <otto.laitinen@edu.turkuamk.fi>
2022-02-02 12:10:35 +0200

    modify components

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+
+import useStyles from './ListStyles';
+
+const places = [
+    { name: 'Some place' },
+    { name: 'Some other place' },
+    { name: 'Some third place' },
+    { name: 'Some place' },
+    { name: 'Some other place' },
+    { name: 'Some third place' },
+    { name: 'Some place' },
+    { name: 'Some other place' },
+    { name: 'Some third place' }
+];
 
 const List = () => {
+    const classes = useStyles();
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
     return (
-        <h1>Listt</h1>
+        <div className={classes.container}>
+            <Typography variant='h4'>Ravintolat, hotellit ja nähtävyydet</Typography>
+            <FormControl className={ classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Ravintolat</MenuItem>
+                    <MenuItem value="hotels">Hotellit</MenuItem>
+                    <MenuItem value="attractions">Nähtävyydet</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={ classes.formControl}>
+                <InputLabel>Arvostelu</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>Kaikki</MenuItem>
+                    <MenuItem value={3}>Yli 2.0</MenuItem>                    
+                    <MenuItem value={3}>Yli 3.0</MenuItem>
+                    <MenuItem value={4}>Yli 4.0</MenuItem>
+                    <MenuItem value={4.5}>Yli 4.5</MenuItem>
+                </Select>
+            </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid item key={i} xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>            
+        </div>
     )
 }
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlineIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './MapStyles';
 
 const Map = () => {
+    const classes = useStyles();
+    // if the width of the device is > 600px, it is not considered a mobile device
+    const isMobile = useMediaQuery('(min-width:600px)')
+    const coordinates = { lat: 0, lng: 0 }
     return (
-        <h1>Mapp</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBm_74cKUbqBGtgjjyQFW5VNgI3MuaZB40' }} // API key
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     )
 }
 
