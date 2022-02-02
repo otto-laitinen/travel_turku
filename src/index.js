ad16143d17fc0ec7cb42ae9fd3b55a41b87026d7
Otto <otto.laitinen@edu.turkuamk.fi>
2022-02-02 09:48:42 +0200

    initialize app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//./ is relative path to something in current directory. The dot is current directory and slash is path delimiter.
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Every React application needs to be mounted onto the root div:
+ReactDOM.render(<App />, document.getElementById('root'));
